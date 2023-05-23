a6f2b5b64219a1e73a7fddb5c702dd826f2fa1f0
Drew Bidmead <dmbidmead@gmail.com>
2023-05-23 16:25:02 -0400

    Mobile styling and menu functionality implemented

--- app.js
+++ app.js
@@ -2,16 +2,39 @@
 const productToggle = document.querySelector('#product-toggle');
 productToggle.dropdown = document.querySelector('#product-dropdown');
 productToggle.dropdownHeight = getComputedStyle(productToggle.dropdown).height;
+productToggle.dropdown.style.height = '0px';
+productToggle.dropdown.style.padding = '0px';
 
 const companyToggle = document.querySelector('#company-toggle');
 companyToggle.dropdown = document.querySelector('#company-dropdown');
 companyToggle.dropdownHeight = getComputedStyle(companyToggle.dropdown).height;
+companyToggle.dropdown.style.height = '0px';
+companyToggle.dropdown.style.padding = '0px';
 
 const connectToggle = document.querySelector('#connect-toggle');
 connectToggle.dropdown = document.querySelector('#connect-dropdown');
 connectToggle.dropdownHeight = getComputedStyle(connectToggle.dropdown).height;
+connectToggle.dropdown.style.height = '0px';
+connectToggle.dropdown.style.padding = '0px';
 
 const toggles = [productToggle, companyToggle, connectToggle];
+
+const mobileMenu = document.querySelector('.dropdown-container');
+mobileMenu.initialHeight = getComputedStyle(mobileMenu).height;
+mobileMenu.style.height = '0px';
+mobileMenu.style.padding = '0 2rem';
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    mobileMenu.style.height = mobileMenu.classList.contains('active') ?
+        mobileMenu.initialHeight :
+        '0px';
+    mobileMenu.setAttribute('style', mobileMenu.classList.contains('active') ?
+        'padding: 2rem' :
+        'padding: 0 2rem');
+})
 
 function toggleDropdown(e) {
     toggles.forEach(toggle => {
